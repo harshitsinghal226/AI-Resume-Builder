fb9c1828ac9ba4737265de69aa855fb0aaf5a42a
harshitsinghal226 <harshitsinghal226@gmail.com>
2026-02-01 03:03:31 +0530

    first commit

---
+++ backend/routes/resumeRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const {
+  createResume,
+  getUserResumes,
+  getResumeById,
+  updateResume,
+  deleteResume,
+} = require("../controllers/resumeController");
+
+const { protect } = require("../middlewares/authMiddleware");
+const { uploadResumeImages } = require("../controllers/uploadImages");
+
+const router = express.Router();
+
+router.post("/", protect, createResume); // Create Resume
+router.get("/", protect, getUserResumes); // Get all resumes for a user
+router.get("/:id", protect, getResumeById); // Get resume by ID
+router.put("/:id", protect, updateResume); // Update resume 
+router.put("/:id/upload-images", protect, uploadResumeImages);
+
+router.delete("/:id", protect, deleteResume); // Delete resume
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const resumeRoutes = require("./routes/resumeRoutes");
+
+const app = express();
+
+//Middleware to handle CORS
+app.use(cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
+// Connect to Database
+connectDB();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/resume", resumeRoutes);
+
+// Serve uploads folder
+app.use(
+    "/uploads",
+    express.static(path.join(__dirname, "uploads"), {
+        setHeaders: (res, path) => {
+            res.set("Access-Control-Allow-Origin", "http://localhost:5173");
+        },
+    })
+);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+});
